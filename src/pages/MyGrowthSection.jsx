9b288bcd988066545d76dde621d1a88284923c11
jabo arnold landry <arnoldjab@gmail.com>
2025-08-30 21:40:46 +0200

    feat adding two remainingcards
    in this commit I added the reamining cards for testing I will refactor the codes when I have a solid design

--- src/pages/MyGrowthSection.jsx
+++ src/pages/MyGrowthSection.jsx
@@ -1,3 +1,4 @@
+// import { } from  'react-icons';
 export default function MyGrowthSection() {
   return (
     <>
@@ -15,6 +16,34 @@ export default function MyGrowthSection() {
         <button className="text-white bg-primary rounded-[0.422rem] p-2 col-span-full capitalize">
           view full profile
         </button>
+      </div>
+
+      <div className="bg-white-solid rounded-md m-4 px-2 py-1 border border-gray-200">
+        <strong className="font-bold capitalize justify-self-start">
+          recent badges
+        </strong>
+        <div className="flex gap-5">
+          <img src="" alt="reward-icon" />
+          <div className="info">
+            <strong>Silver Achiever</strong>
+            <p className="text-azure-34">achieved last 7 days</p>
+          </div>
+        </div>
+      </div>
+      <div className="grid gap-3 bg-white-solid rounded-md m-4 px-2 py-1 border border-gray-200">
+        <strong className="font-bold capitalize">quick actions</strong>
+        <div className="flex gap-3 border border-progress rounded-sm px-2 py-1 cursor-pointer">
+          <span>+</span>
+          <button>add project to showcase</button>
+        </div>
+        <div className="flex gap-3 border border-progress rounded-sm px-2 py-1 cursor-pointer">
+          <span>+</span>
+          <button>request testimony</button>
+        </div>
+        <div className="flex gap-3 border border-progress rounded-sm px-2 py-1 cursor-pointer">
+          <span>+</span>
+          <button>view leaderboard</button>
+        </div>
       </div>
     </>
   );
