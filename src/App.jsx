8c6a5a73f7ac22a200cf2c7a227357f9fd37e15d
Zerro2003 <mugishaelie160@gmail.com>
2025-09-01 19:55:59 +0200

    Create student dashbord growth

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-
-import React from 'react';
-import Navbar from './components/Navbar';
-import Welcome  from './components/Welcome';
-import MyGrowthSection from "./pages/MyGrowthSection";  
+import React from "react";
+import Navbar from "./components/Navbar";
+import Welcome from "./components/Welcome";
+import MyGrowthSection from "./pages/MyGrowthSection";
 import TabComponent from "./competition.jsx";
 function App() {
   return (
     <>
       <Navbar />
-      <Welcome /> 
-      <MyGrowthSection />
+      <Welcome />
       <TabComponent />
+      <MyGrowthSection />
     </>
   );
 }
