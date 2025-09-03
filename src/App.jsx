88ec2a7d35ba7a0b2bb1ae9142f64ccac10ba45d
Ange290 <uange209@gmail.com>
2025-09-03 21:55:30 +0200

    crate impact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Dashboard from "./Components/Dashboard";
+import ImpactCard from "./Components/Impact";
+import RecentActivity from "./Components/RecentActivity";
+import CurriculumSuggestion from "./Components/curriculum";
 
 function App() {
-  return (
-    <>
-
-      <Dashboard />
-    </>
+ return (
+    <div className="flex justify-end p-5">
+    
+      <div className="w-full sm:w-1/2 lg:w-1/3">
+      <ImpactCard />
+        <RecentActivity />
+        < CurriculumSuggestion/>
+      </div>
+    </div>
   );
-}
+};
+
 
 export default App;
