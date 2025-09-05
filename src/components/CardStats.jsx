c63bcc4fe6d7771cbc6c526072a53985a5bc18ef
jabo arnold landry <arnoldjab@gmail.com>
2025-09-05 05:41:54 +0200

    style:adding-basic-styles-for-the-aside-card-component

--- src/components/CardStats.jsx
+++ src/components/CardStats.jsx
@@ -3,33 +3,33 @@ import { Award } from "lucide-react";
 function CardStats() {
   return (
     <>
-      <div className="card-one">
+      <div className="flex gap-2 mx-3">
         <Award />
         <Award />
         <Award />
       </div>
-      <div className="card-two">
+      <div className="">
         <strong>key metrics</strong>
-        <section>
+        <section className="columns columns-2 gap-3">
           <h4>total points</h4>
           <p>343</p>
         </section>
-        <section>
+        <section className="columns columns-2 gap-3">
           <h4>challenge completed</h4>
           <p>34</p>
         </section>
-        <section>
+        <section className="columns columns-2 gap-3">
           <h4>hackathon won</h4>
           <p>43</p>
         </section>
-        <section>
+        <section className="columns columns-2 gap-3">
           <h4>showcased project</h4>
           <p>10</p>
         </section>
       </div>
-      <div className="card-three">
+      <div className="grid">
         <strong>Next milestone</strong>
-        <div>
+        <div className="justify-self-center">
           <Award />
           <h3>Gold achiever</h3>
           <p>conglatulations</p>
