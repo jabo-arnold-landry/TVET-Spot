8619f6531872e7cecc3cbf701fd7ad32fadf4cf5
jabo arnold landry <arnoldjab@gmail.com>
2025-09-05 12:17:20 +0200

    style:adding-styles-and-layout-to-cards-and-their-contents

--- src/components/CardStats.jsx
+++ src/components/CardStats.jsx
@@ -3,38 +3,52 @@ import { Award } from "lucide-react";
 function CardStats() {
   return (
     <>
-      <div className="flex gap-2 mx-3">
-        <Award />
-        <Award />
-        <Award />
+      <div className="bg-white px-3 py-5 rounded">
+        <strong className="">Badge collection</strong>
+        <div className="flex justify-between items-center">
+          <span className="grid gap-1.5">
+            <Award className="justify-self-center size-10 bg-amber-500 text-white px-3 py-1 rounded-full" />
+            <p>Bronze</p>
+          </span>
+          <span>
+            <Award className="justify-self-center size-10 bg-orange-500 text-white px-3 py-1 rounded-full" />
+            <p>Gold</p>
+          </span>
+          <span>
+            <Award className="justify-self-center size-10 bg-gray-300 text-white px-3 py-1 rounded-full" />
+            <p>silver</p>
+          </span>
+        </div>
       </div>
-      <div className="">
+      <div className="bg-white px-3 py-5 rounded">
         <strong>key metrics</strong>
-        <section className="columns columns-2 gap-3">
+        <section className="flex gap-2 justify-between">
           <h4>total points</h4>
           <p>343</p>
         </section>
-        <section className="columns columns-2 gap-3">
-          <h4>challenge completed</h4>
-          <p>34</p>
+        <section className="flex justify-between">
+          <h4>total points</h4>
+          <p>343</p>
         </section>
-        <section className="columns columns-2 gap-3">
-          <h4>hackathon won</h4>
-          <p>43</p>
+        <section className="flex justify-between">
+          <h4>total points</h4>
+          <p>343</p>
         </section>
-        <section className="columns columns-2 gap-3">
-          <h4>showcased project</h4>
-          <p>10</p>
+        <section className="flex justify-between">
+          <h4>total points</h4>
+          <p>343</p>
         </section>
       </div>
-      <div className="grid">
+      <div className="bg-white px-3 py-5 rounded relative">
         <strong>Next milestone</strong>
-        <div className="justify-self-center">
-          <Award />
-          <h3>Gold achiever</h3>
-          <p>conglatulations</p>
+        <div className="grid place-items-center">
+          <Award className="size-10 bg-orange-500 text-white px-3 py-1 rounded-full" />
+          <h3 className="font-bold text-xl">Gold achiever</h3>
+          <p className="justisfy-self-center text-azure-34 text-md">
+            conglatulations
+          </p>
         </div>
-        <div></div>
+        <div className="bg-red-400 absolute"></div>
       </div>
     </>
   );
