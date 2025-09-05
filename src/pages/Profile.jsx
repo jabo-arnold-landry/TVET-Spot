0fdcee53f6e67acb59c96104c254eb6809f1ee59
jabo arnold landry <arnoldjab@gmail.com>
2025-09-05 12:16:49 +0200

    style:enhancing-the-layout-of-the-showcase-section-and-the-student-card-stats

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -58,63 +58,66 @@ function Profile() {
     <>
       <strong>Growth Profile</strong>
       <p>track your learning journey and achievements</p>
-      <div className="grid gap-5 sm:grid-cols-3">
-        <main onClick={SetHackathonsTab} className="grid gap-3 col-span-2">
-          <div className="flex gap-10 cursor-pointer">
-            <span
-              id="showcase-section"
-              className={
-                !activeTab
-                  ? "border-b-8 border-b-primary w-fit text-primary font-bold"
-                  : "font-light text-azure-34 capitalize mb-3"
-              }
-            >
-              showcase
-            </span>
-            <span
-              id="hackathon-section"
-              className={
-                activeTab
-                  ? "border-b-8 border-b-primary w-fit text-primary font-bold"
-                  : "font-light text-azure-34 capitalize mb-3"
-              }
-            >
-              hackathon
-            </span>
-          </div>
+
+      <main onClick={SetHackathonsTab} className="grid gap-3 col-span-2">
+        <div className="flex gap-10 cursor-pointer">
+          <button
+            id="showcase-section"
+            className={
+              !activeTab
+                ? "border-b-8 border-b-primary w-fit text-primary font-bold"
+                : "font-light text-azure-34 capitalize mb-3"
+            }
+          >
+            showcase
+          </button>
+          <button
+            id="hackathon-section"
+            className={
+              activeTab
+                ? "border-b-8 border-b-primary w-fit text-primary font-bold"
+                : "font-light text-azure-34 capitalize mb-3"
+            }
+          >
+            hackathon
+          </button>
+        </div>
+        <section className="grid grid-cols-1 sm:grid-cols-[2fr_1fr] gap-4">
           <article className="grid gap-2">
             {!activeTab ? (
               <LinkPreview />
             ) : (
               <ChallengesCard challenges={hackathon} />
             )}
+            <section className="bg-white px-2.5 py-0.5 rounded">
+              <strong>Recent Notification</strong>
+              <RecentNotification notifications={recentUpdates} />
+            </section>
           </article>
-          <section className="notifacation mx-3 col-span-full">
-            <strong>Recent Notification</strong>
-            <RecentNotification notifications={recentUpdates} />
-          </section>
-        </main>
-        <aside>
-          <CardStats />
-        </aside>
-      </div>
+          <aside className="mx-3 grid gap-2">
+            <CardStats />
+          </aside>
+        </section>
+      </main>
     </>
   );
 }
 function LinkPreview() {
   return (
-    <Microlink
-      url="https://www.youtube.com/watch?v=twDtPs5NCjA"
-      contrast
-      autoPlay
-      media="video"
-      setData={(data) => ({
-        ...data,
-        title: "helloword",
-        description: "fjdkfsfsdlfsdfklsdfsdkfsdf",
-        image: { url: "https://i.imgur.com/1FyFxlk.jpg" },
-      })}
-    />
+    <>
+      <Microlink
+        url="https://www.youtube.com/watch?v=twDtPs5NCjA"
+        contrast
+        autoPlay
+        media="video"
+        setData={(data) => ({
+          ...data,
+          title: "helloword",
+          description: "fjdkfsfsdlfsdfklsdfsdkfsdf",
+          image: { url: "https://i.imgur.com/1FyFxlk.jpg" },
+        })}
+      />
+    </>
   );
 }
 export default Profile;
