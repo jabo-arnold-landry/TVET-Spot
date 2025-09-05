ad7131866677f83b612370def7197752cc3d2ad8
jabo arnold landry <arnoldjab@gmail.com>
2025-09-05 04:58:59 +0200

    feat:creating-recent-notification-component
    I created this component for more modular code design and to make the profile file less populated with different things that are not related I am making sure that it job is to render not process the information on the a certain feature that why I created this file

---
+++ src/components/RecentNotification.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card";
+import { Award } from "lucide-react";
+function RecentNotification({ notifications }) {
+  return (
+    <>
+      {notifications.length ? (
+        notifications.map((notification, index) => {
+          const { achivement, title, description } = notification;
+          return (
+            <div className="flex items-center bg-gray-200" key={index}>
+              <Award className="size-10" />
+              <div>
+                <strong>{title}</strong>
+                <p>{description}</p>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <h2>No recent notifications found</h2>
+      )}
+    </>
+  );
+}
+
+export default RecentNotification;
