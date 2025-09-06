c242f6d923dd3e40792f52f901797ed694e557ea
jabo arnold landry <arnoldjab@gmail.com>
2025-09-06 11:00:36 +0200

    fix:fix-the-page-alignment-margins-specifically

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -75,46 +75,47 @@ function Profile() {
         track your learning journey and achievements
       </p>
 
-      <main onClick={SetHackathonsTab} className="grid gap-3 col-span-2">
-        <div className="flex gap-10 cursor-pointer my-2">
-          <button
-            id="showcase-section"
-            className={
-              !activeTab
-                ? "border-b-8 border-b-primary w-fit text-primary font-bold"
-                : "font-light text-azure-34 capitalize mb-3"
-            }
-          >
-            showcase
-          </button>
-          <button
-            id="hackathon-section"
-            className={
-              activeTab
-                ? "border-b-8 border-b-primary w-fit text-primary font-bold"
-                : "font-light text-azure-34 capitalize mb-3"
-            }
-          >
-            hackathon
-          </button>
-        </div>
-        <section className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-          <article className="grid gap-2 sm:col-span-2">
-            {!activeTab ? (
-              <LinkPreview />
-            ) : (
-              <ChallengesCard challenges={hackathon} />
-            )}
-            <section className="bg-white px-2.5 py-0.5 rounded grid gap-1.5">
-              <strong>Recent Notification</strong>
-              <RecentNotification notifications={recentUpdates} />
-            </section>
-          </article>
-          <aside className="mx-3 grid gap-2">
-            <CardStats />
-          </aside>
-        </section>
-      </main>
+        <main onClick={SetHackathonsTab} className="grid gap-3 col-span-2">
+          <div className="flex gap-10 cursor-pointer my-2">
+            <button
+              id="showcase-section"
+              className={
+                !activeTab
+                  ? "border-b-8 border-b-primary w-fit text-primary font-bold"
+                  : "font-light text-azure-34 capitalize mb-3"
+              }
+            >
+              showcase
+            </button>
+            <button
+              id="hackathon-section"
+              className={
+                activeTab
+                  ? "border-b-8 border-b-primary w-fit text-primary font-bold"
+                  : "font-light text-azure-34 capitalize mb-3"
+              }
+            >
+              hackathon
+            </button>
+          </div>
+          <section className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            <article className="grid gap-2 sm:col-span-2">
+              {!activeTab ? (
+                <LinkPreview />
+              ) : (
+                <ChallengesCard challenges={hackathon} />
+              )}
+              <section className="bg-white px-2.5 py-0.5 rounded grid gap-1.5">
+                <strong>Recent Notification</strong>
+                <RecentNotification notifications={recentUpdates} />
+              </section>
+            </article>
+            <aside className="mx-3 grid gap-2">
+              <CardStats />
+            </aside>
+          </section>
+        </main>
+      </div>
     </>
   );
 }
@@ -123,15 +124,15 @@ function LinkPreview() {
     <>
       <Microlink
         url="https://www.youtube.com/watch?v=twDtPs5NCjA"
-        contrast
         autoPlay
         media="video"
-        setData={(data) => ({
-          ...data,
-          title: "helloword",
-          description: "fjdkfsfsdlfsdfklsdfsdkfsdf",
-          image: { url: "https://i.imgur.com/1FyFxlk.jpg" },
-        })}
+        size="small"
+      />
+      <Microlink
+        url="https://www.youtube.com/watch?v=twDtPs5NCjA"
+        autoPlay
+        media="video"
+        size="small"
       />
     </>
   );
