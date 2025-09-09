9fd14d04c5ef9d0e9f8e9d5e93787c3c93514cb5
jabo arnold landry <arnoldjab@gmail.com>
2025-09-09 13:12:43 +0200

    feat:add-link-parser-function
    added the API to help get the provided link so we can use it to customize on out style and design

--- src/pages/MyGrowthSection.jsx
+++ src/pages/MyGrowthSection.jsx
@@ -88,6 +88,28 @@ export default function MyGrowthSection() {
 
 function Showcase({ isOpen, onClose }) {
   const [link, setLink] = useState("");
+  const [urlInfo, seturlinfo] = useState([]);
+  const apiKey = "pk_01dfcefda77adf8d9e22acd16e5be2038b8c737a";
+  async function parseLink(url) {
+    if (!url) {
+      return alert("please input the link");
+    }
+    try {
+      const response = await fetch(
+        `https://jsonlink.io/api/extract?url=${url}&api_key=${apiKey}`
+      );
+      const data = await response.json();
+      if (!response.ok || data.error) {
+        throw new Error(
+          `failed to get the link's data make sure you are providing the correct link`
+        );
+      }
+      seturlinfo((prev) => [...prev, data]);
+    } catch (err) {
+      alert(err.message);
+      console.log("something wrong occured:", err);
+    }
+  }
 
   return (
     <AnimatePresence>
@@ -121,8 +143,8 @@ function Showcase({ isOpen, onClose }) {
                 disabled={!link}
                 onClick={async (e) => {
                   e.preventDefault();
-                  // await settingItems({ url: link, cors: false });
-                  // setLink("");
+                  parseLink(link);
+                  await settingItems(...urlInfo);
                   setLink("");
                   onClose();
                 }}
